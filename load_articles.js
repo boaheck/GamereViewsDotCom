afa5f64cc378db8fb2c0c2ed98ca1af5acabc74c
Breogán Hackett <breogan.hackett2@mail.dcu.ie>
2016-05-29 19:07:36 +0100

    Testing load_articles.js

--- load_articles.js
+++ load_articles.js
@@ -1,19 +1,15 @@
 function test() {
 	var tfile = new XMLHttpRequest();
-	tfile.open("GET","test.json");
+	tfile.open("GET","test.json",true);
+	tfile.send();
 	tfile.onreadystatechange = function() {
-		if(tfile.readyState === 4){
-			if (tfile.status === 200) {
-				var tjson = JSON.parse(tfile.responseText);
-				var elem = document.getElementById("about");
-				elem.innerHTML = tjson.test;
-				console.log(tjson.test);
-				console.log(tjson.type)
-			}
+		if(tfile.readyState === 4 && tfile.status === 200) {
+			var tjson = JSON.parse(tfile.responseText);
+			var elem = document.getElementById("about");
+			elem.innerHTML = tjson.test;
+			console.log(tjson.test);
+			console.log(tjson.type);
 		}
 	};
-	while(tfile.readyState !== 4){
-		console.log(tfile.readyState);
-	}
 	console.log("run");
 }
