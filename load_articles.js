a10d2545a4988a3d36bcad99d046396b16d99686
Breogán Hackett <breogan.hackett2@mail.dcu.ie>
2016-05-29 19:18:56 +0100

    messed about with javascript...

--- load_articles.js
+++ load_articles.js
@@ -1,15 +1,15 @@
 function test() {
 	var tfile = new XMLHttpRequest();
-	tfile.open("GET","test.json",true);
-	tfile.send();
 	tfile.onreadystatechange = function() {
 		if(tfile.readyState === 4 && tfile.status === 200) {
-			var tjson = JSON.parse(tfile.responseText);
+			var tjson = JSON.parse(tfile.responseJSON);
 			var elem = document.getElementById("about");
 			elem.innerHTML = tjson.test;
 			console.log(tjson.test);
 			console.log(tjson.type);
 		}
 	};
+	tfile.open("GET","test.json",true);
+	tfile.send();
 	console.log("run");
 }
