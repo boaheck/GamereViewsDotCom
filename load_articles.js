b4bffb9160ae814a16c1f7d275e3955f92f357fe
Breogán Hackett <breogan.hackett2@mail.dcu.ie>
2016-05-29 19:21:05 +0100

    fixed things

--- load_articles.js
+++ load_articles.js
@@ -2,7 +2,7 @@ function test() {
 	var tfile = new XMLHttpRequest();
 	tfile.onreadystatechange = function() {
 		if(tfile.readyState === 4 && tfile.status === 200) {
-			var tjson = JSON.parse(tfile.responseJSON);
+			var tjson = JSON.parse(tfile.responseText);
 			var elem = document.getElementById("about");
 			elem.innerHTML = tjson.test;
 			console.log(tjson.test);
