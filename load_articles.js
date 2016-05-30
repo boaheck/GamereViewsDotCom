c8b2b8d0eb4054b58ef845f43095b1cecfb18e02
Breogán Hackett <breogan.hackett2@mail.dcu.ie>
2016-05-30 22:05:31 +0100

    Improve article assignment

--- load_articles.js
+++ load_articles.js
@@ -8,35 +8,33 @@ function loadArticleURL(){
 }
 
 function loadArticle(articleID, ParentID){
-	getJSONFile("articles\\ReviewIndex.json",function(reviewIndex){
+	getJSONFile("articles\\ReviewIndex.json","",function getArticle(reviewIndex)
 	console.log(reviewIndex);
 	if(articleID >= reviewIndex.articles.length){
 		articleID = 0;
 	}
 	var file = reviewIndex.articles[articleID].file;
-	getJSONFile("articles\\"+file,function(review){
+	getJSONFile("articles\\"+file,ParentID,assignArticle);
+	}
+}
+
+function assignArticle(json,ParentID){
 	var htmlArticle = document.getElementById(ParentID);
 	htmlArticle.getElementsByClassName("articlehead")[0].innerHTML=review.articletitle;
 	htmlArticle.getElementsByClassName("articleimg")[0].setAttribute("src","img\\"+review.img);
 	htmlArticle.getElementsByClassName("articlescore")[0].innerHTML=review.score;
 	htmlArticle.getElementsByClassName("articledate")[0].innerHTML=review.date;
 	htmlArticle.getElementsByClassName("articlebody")[0].innerHTML=review.bod;
-	});
-	});
 }
 
-function getJSONFile(file, callback){
+function getJSONFile(file, other, callback){
 	var xmlhttp = new XMLHttpRequest();
 	xmlhttp.onreadystatechange = function() {
 		if(xmlhttp.readyState === 4 && xmlhttp.status === 200) {
 			var rjson = JSON.parse(xmlhttp.responseText);
-			console.log(rjson);
-			console.log("Got " + file);
-			callback(rjson);
+			callback(rjson,other);
 		}
 	};
 	xmlhttp.open("GET",file,true);
 	xmlhttp.send();
-	console.log("Getting " + file);
-	
 }
