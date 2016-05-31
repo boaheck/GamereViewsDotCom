4d8bd822a36e2be99e982db56d1ffa7e7d852fd0
Breogán Hackett <breogan.hackett2@mail.dcu.ie>
2016-05-31 12:55:54 +0100

    Test page loading

--- load_articles.js
+++ load_articles.js
@@ -1,3 +1,5 @@
+var pageLength = 5;
+
 function loadArticleURL(){
 	if(location.search.length <= 1){
 		location.search = "?0";
@@ -5,6 +7,27 @@ function loadArticleURL(){
 	var s = location.search;
 	var id = parseInt(s.slice(1));
 	loadArticle(id,"articles");
+}
+
+function loadPageURL(){
+	if(location.search.length <= 1){
+		location.search = "?0";
+	}
+	var s = location.search;
+	var page = parseInt(s.slice(1));
+	var startical = (page * pageLength);
+	getJSONFile("articles\\ReviewIndex.json","",function getArticles(reviewIndex){
+		var len = reviewIndex.articles.length;
+		for (var i = 0; i < pageLength; i++) {
+			articleID = len - (startical + i);
+			if(articleID < 0){
+				removeElement(document.getElementById("article"+(i+1)));
+			}
+			var file = reviewIndex.articles[articleID].file;
+			getJSONFile("articles\\"+file,ParentID,assignPageArticle);
+		};
+		
+	});
 }
 
 function loadArticle(articleID, ParentID){
@@ -17,6 +40,16 @@ function loadArticle(articleID, ParentID){
 	});
 }
 
+function assignArticle(json,ParentID){
+	var htmlArticle = document.getElementById(ParentID);
+	htmlArticle.getElementsByClassName("articlehead")[0].innerHTML=json.articletitle;
+	htmlArticle.getElementsByClassName("articleheaderlink")[0].setAttribute("href","article.html?"+json.num)
+	htmlArticle.getElementsByClassName("articleimg")[0].setAttribute("src","img\\"+json.img);
+	htmlArticle.getElementsByClassName("articlescore")[0].innerHTML=json.score;
+	htmlArticle.getElementsByClassName("articledate")[0].innerHTML=json.date;
+	htmlArticle.getElementsByClassName("mp_articlebody")[0].innerHTML=json.bod;
+	htmlArticle.getElementsByClassName("articleReadmore")[0].setAttribute("href","article.html?"+json.num)
+}
 function assignArticle(json,ParentID){
 	var htmlArticle = document.getElementById(ParentID);
 	htmlArticle.getElementsByClassName("articlehead")[0].innerHTML=json.articletitle;
@@ -36,4 +69,8 @@ function getJSONFile(file, other, callback){
 	};
 	xmlhttp.open("GET",file,true);
 	xmlhttp.send();
+}
+
+function removeElement(elem){
+	elem.parentElement.removeChild(elem);
 }
